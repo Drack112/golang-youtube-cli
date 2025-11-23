9129443dfc8fb51471bc644b96b7510e2c55daea
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-11-23 17:54:40 -0300

    feat: implement TUI app on the main

--- cmd/go-youtube/main.go
+++ cmd/go-youtube/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Drack112/go-youtube/internal/flags"
-	"github.com/Drack112/go-youtube/internal/handlers"
+	"github.com/Drack112/go-youtube/internal/tui"
 	"github.com/Drack112/go-youtube/pkg/logger"
 )
 
 func main() {
-	value, err := flags.ParseFlags()
+	opts, err := flags.ParseFlags()
 	if err != nil {
-		if err == flags.ErrHelpRequested {
+		switch err {
+		case flags.ErrHelpRequested:
 			return
+		default:
+			logger.Fatal(flags.ErrorHandler(err))
 		}
-		logger.Fatal(flags.ErrorHandler(err))
 	}
 
-	fmt.Println(handlers.SearchWithRetries(value))
+	// For interactive TUI
+	model := tui.NewModel(opts)
+	if err := tui.NewProgram(model).Start(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 }
