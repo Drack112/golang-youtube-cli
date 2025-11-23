0bfdf9b58f8af53ad2c46029c142c7578005bd01
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-11-23 17:53:17 -0300

    feat: allow log be visualized by a other terminal popuip instead in the main instance

---
+++ internal/player/logwindow.go
@@ -0,0 +1,34 @@
+package player
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func createLogFile() (*os.File, string, error) {
+	tmpDir := os.TempDir()
+	logPath := filepath.Join(tmpDir, "go-youtube.log")
+
+	logFile, err := os.Create(logPath)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return logFile, logPath, nil
+}
+
+func openLogWindow(logPath string) error {
+	var cmd *exec.Cmd
+
+	if _, err := exec.LookPath("mate-terminal"); err == nil {
+		cmd = exec.Command("mate-terminal", "--", "tail", "-f", logPath)
+	} else if _, err := exec.LookPath("xterm"); err == nil {
+		cmd = exec.Command("xterm", "-e", fmt.Sprintf("tail -f %s", logPath))
+	} else {
+		return fmt.Errorf("no supported terminal emulator found")
+	}
+
+	return cmd.Start()
+}
