8318a7f17fef6c5b039f0834a1a7b73072e92a2b
João Vitor <61944386+Drack112@users.noreply.github.com>
2025-11-23 15:16:50 -0300

    feat: parser for the flags with quality and input of url and values

--- internal/flags/parser.go
+++ internal/flags/parser.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/Drack112/go-youtube/pkg/logger"
 	"github.com/Drack112/go-youtube/pkg/utils"
+	"github.com/charmbracelet/huh"
 )
 
 var (
@@ -14,6 +16,8 @@ var (
 	ErrHelpRequested     = errors.New("help requested")
 	ErrDownloadRequested = errors.New("download requested")
 	ErrNoInput           = errors.New("no input provided")
+
+	GlobalQuality string
 )
 
 type InputSrc int
@@ -40,32 +44,62 @@ func ErrorHandler(err error) string {
 	return fmt.Sprintf("%v (run with -debug for details)", err)
 }
 
+func getUserInput() (string, error) {
+	var videoName string
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().Title("Enter video or topic bellow").Description("Type a Youtube URL or a search term").Value(&videoName).Validate(func(v string) error {
+				if len(strings.TrimSpace(v)) < 3 {
+					return fmt.Errorf("input must have at least 3 characters")
+				}
+				return nil
+			}),
+		),
+	)
+
+	if err := form.Run(); err != nil {
+		return "", err
+	}
+
+	return videoName, nil
+}
+
 func ParseFlags() (*Options, error) {
 	opts := &Options{}
 
-	flag.BoolVar(&opts.Debug, "debug", false, "enable debug mode")
-	flag.BoolVar(&opts.Download, "download", false, "download instead of streaming")
-	flag.StringVar(&opts.Quality, "quality", "best", "video quality (best, worst, 720p, 1080p)")
+	debug := flag.Bool("debug", false, "enable debug mode")
+	download := flag.Bool("download", false, "download instead of stream")
+	quality := flag.String("quality", "best", "video quality (best, worst, 720p, 1080p)")
+
+	help := flag.Bool("help", false, "show help message")
+	versionFlag := flag.Bool("version", false, "show version information")
 
 	flag.Usage = func() {
-		fmt.Println("\ngo-youtube [OPTIONS] <url | search term | video name>")
+		fmt.Println("\ngo-youtube [OPTIONS] <url | search term>")
 		fmt.Println("Examples:")
-		fmt.Println("  go-youtube \"funny cats\"")
-		fmt.Println("  go-youtube https://youtu.be/xyz")
+		fmt.Println("  go-youtube \"lofi chill\"")
+		fmt.Println("  go-youtube https://youtu.be/xxx")
 		fmt.Println("  go-youtube -download \"metallica live\"")
 		fmt.Println("\nOptions:")
 		flag.PrintDefaults()
-		fmt.Println()
 	}
-
-	help := flag.Bool("help", false, "show help message")
-	version := flag.Bool("version", false, "show version information")
 
 	flag.Parse()
 
-	IsDebug = opts.Debug
+	opts.Debug = *debug
+	opts.Download = *download
+	opts.Quality = *quality
 
-	if *version || utils.HasVersionArg() {
+	IsDebug = opts.Debug
+	GlobalQuality = opts.Quality
+
+	if opts.Debug {
+		logger.InitLogger(opts.Debug)
+		logger.Debug("Debug mode enabled")
+	}
+
+	if *versionFlag || utils.HasVersionArg() {
 		utils.ShowVersion()
 		return nil, ErrHelpRequested
 	}
@@ -76,24 +110,34 @@ func ParseFlags() (*Options, error) {
 	}
 
 	args := flag.Args()
-	if len(args) == 0 {
-		flag.Usage()
+	var input string
+
+	if len(args) > 0 {
+		input = strings.Join(args, " ")
+	} else {
+		in, err := getUserInput()
+		if err != nil {
+			return nil, err
+		}
+		input = utils.CleanYoutubeLink(in)
+	}
+
+	input = strings.TrimSpace(input)
+	if input == "" {
 		return nil, ErrNoInput
 	}
 
-	input := strings.Join(args, "")
-	opts.Input = strings.TrimSpace(input)
+	opts.Input = input
 
-	if utils.IsYoutubeURL(opts.Input) {
+	if utils.IsYouTubeURL(input) {
 		opts.InputKind = InputYoutubeURL
 	} else {
 		opts.InputKind = InputSearchQuery
 	}
 
 	if opts.Download {
-		return nil, ErrDownloadRequested
+		return opts, ErrDownloadRequested
 	}
 
 	return opts, nil
-
 }
